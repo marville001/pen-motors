2ddee03fb80270b139d6cc9080bfd872ed6e08ad
marville001 <mwangimartin1904@gmail.com>
2021-08-23 10:54:43 +0300

    Added header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import CarDetails from './pages/CarDetails'
-import Cart from './pages/Cart'
-import Home from './pages/Home'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import CarDetails from "./pages/CarDetails";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
 
 const App = () => {
-    return (
-        <div>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/details" component={CarDetails}/>
-                <Route path="/cart" component={Cart}/>
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/details" component={CarDetails} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </div>
+  );
+};
 
-export default App
+export default App;
